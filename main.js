eb6f177208f4c58a627a11f83a9f4f7146139ad5
damirusso95 <damirusso95@gmail.com>
2024-05-14 17:27:39 +0200

    aggiungo icone e provo a rimuovere input

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 // Creare una mini applicazione Vue che ci permetta di visualizzare una semplice ToDo List.
 
-// :roccia: MILESTONE 1
-// Aggiungete un <input> il cui value sia collegato ad un data (ricordate come si fa?).
-// Aggiungete un pulsante che al click stampi quel data in console, poi lo aggiunga all'array precedente.
-// Come risultato dovreste aver guadagnato la possibilità di aggiungere elementi alla lista visualizzata in pagina.
-// Ora riuscite a svuotare l'input? :faccia_pensosa:
-// :roccia: MILESTONE 2
+
+
 // Accanto al testo del ToDo aggiungete una "X", ad esempio con FontAwesome.
 // Al click su questo elemento il ToDo corrispondente viene rimosso dalla lista.
 
@@ -13,6 +9,7 @@
 const { createApp } = Vue
 
 createApp({
+
     data() {
         return {
             // Array di cose da fare
@@ -23,27 +20,32 @@ createApp({
                 'Andare in palestra',
                 'Preparare la cena'
             ],
+
             //   qui salvo gli input
             newTodo: ''
 
+
         }
+
     },
+
 
     methods: {
         // nuova cosa da fare all'array todos
-        addTodo(){
-            if (this.newTodo.trim() !== ''){
+        addTodo() {
+            if (this.newTodo.trim() !== '') {
                 // push sull'array
                 this.todos.push(this.newTodo);
                 // Resetta l'input
-          this.newTodo = ''; 
+                this.newTodo = '';
             }
         }
+    },
+    // funzione per rimuovere
+    removeTodo() {
+        
     }
 
-
 },
-
-
 
 ).mount('#app')
